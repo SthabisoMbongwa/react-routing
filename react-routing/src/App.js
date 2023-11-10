42854afb19de585a5a6efedf9f58050fbe9f3839
Sthabiso <sthabiso@aims.ac.za>
2023-11-10 13:37:35 +0200

    Created a products components

--- react-routing/src/App.js
+++ react-routing/src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Products from './pages/Products';
 
 function App() {
   return (
     <Routes>
       <Route path= "/" element={<Home />} />
       <Route path="/About" element={<About />} />
+      <Route path="/products" element={<Products />} />
     </Routes>
   );
 }
---
+++ react-routing/src/pages/Products.js
@@ -0,0 +1,11 @@
+import React from 'react-router-dom';
+
+const Products = function (){
+    return(
+        <div>
+            <h1>This is my product page.</h1>
+        </div>
+    )
+}
+
+export default Products;
