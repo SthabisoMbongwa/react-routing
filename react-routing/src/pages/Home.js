b57e728b7b090b82d7633de5263e877e0c79c5eb
Sthabiso <sthabiso@aims.ac.za>
2023-11-10 13:23:48 +0200

    Created home component and and place it inside pages folder

---
+++ react-routing/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react-router-dom';
+
+const Home = function() {
+    return(
+        <div>
+            <h1>This is my Home pages.</h1>
+        </div>
+    )
+}
+
+export default Home;
